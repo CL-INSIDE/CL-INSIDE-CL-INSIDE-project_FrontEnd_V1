730050da775e0885dbdec3c4d1f798f910e39ad4
추혜연 <hyeyoenchurros@naver.com>
2022-01-04 23:17:27 +0900

    ⚡ :: header style 수정

--- src/components/header/style.ts
+++ src/components/header/style.ts
@@ -10,13 +10,19 @@ export const HeaderTextBox = styled.div`
   margin-left: 10%;
   display: flex;
   justify-content: space-between;
-  align-items: center;
+  // align-items: center;
 `;
 
 export const HeaderText = styled.p`
   color: white;
   cursor: pointer;
+  height: 40px;
+  padding: 9px 16px;
   font-family: "Noto Sans CJK KR";
   font-size: 14px;
   font-weight: 500;
+  :hover {
+    color: #4527a0;
+    background-color: white;
+  }
 `;
