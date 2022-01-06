7d6ae52ff89f3938c356e8eb3e5443af7ae7e2d2
추혜연 <hyeyoenchurros@naver.com>
2022-01-06 21:49:00 +0900

    ⚡ :: mainpage 퍼블리싱

---
+++ src/components/mainpage/index.tsx
@@ -0,0 +1,13 @@
+import * as S from "./style";
+import Login from "../login";
+import Recommend from "../recommend";
+
+const MainPage = () => {
+  return (
+    <>
+      <S.MainPageWrapper></S.MainPageWrapper>
+    </>
+  );
+};
+
+export default MainPage;
