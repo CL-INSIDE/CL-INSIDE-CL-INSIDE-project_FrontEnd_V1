c7f14121b5785322ec217110b39f00feabd2b24f
추혜연 <hyeyoenchurros@naver.com>
2022-01-07 20:21:11 +0900

    ⚡ :: postfeed style 수정

--- src/components/postfeed/index.tsx
+++ src/components/postfeed/index.tsx
@@ -1,7 +1,12 @@
 import * as S from "./style";
 
 export const PostFeed = () => {
-  return <></>;
+  return (
+    <S.PostWrapper>
+      <S.Image></S.Image>
+      <S.Title>크리스마스 쿠키 만들기</S.Title>
+    </S.PostWrapper>
+  );
 };
 
 export default PostFeed;
--- src/components/postfeed/style.ts
+++ src/components/postfeed/style.ts
@@ -1,5 +1,24 @@
 import styled from "styled-components";
 
+export const PostWrapper = styled.div`
+  display: flex;
+  width: 51%;
+  border-bottom-style: solid;
+  border-bottom-color: #707070;
+  border-bottom-width: 1px;
+  padding: 8px;
+  align-items: center;
+`;
+
 export const Image = styled.div`
-    
-`
+  width: 70px;
+  height: 40px;
+  background-color: #b7b7b7;
+`;
+
+export const Title = styled.p`
+  font-family: "Noto Sans CJK KR";
+  font-size: 14px;
+  font-weight: 500;
+  margin-left: 8px;
+`;
