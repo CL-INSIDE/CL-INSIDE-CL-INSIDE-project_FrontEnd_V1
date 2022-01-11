eb7a75a76c4e10d9a0ddd9af48874034feef8306
추혜연 <hyeyoenchurros@naver.com>
2022-01-11 21:16:02 +0900

    ⚡ :: post 퍼블리싱

---
+++ src/components/post/index.tsx
@@ -0,0 +1,11 @@
+import * as S from "./style";
+
+const Post = () => {
+  return (
+    <S.PostWrapper>
+      <S.Title>지구에서 가장 우월한 생물 '고양이'</S.Title>
+    </S.PostWrapper>
+  );
+};
+
+export default Post;
---
+++ src/components/post/style.ts
@@ -0,0 +1,15 @@
+import styled from "styled-components";
+
+export const PostWrapper = styled.div`
+  margin: 0 15%;
+  width: 69%;
+`;
+
+export const Title = styled.p`
+  margin-top: 36px;
+  font-size: 20px;
+  font-weight: 500;
+  color: #4527a0;
+  border-bottom: 2px solid #4527a0;
+  padding-bottom: 8px;
+`;
